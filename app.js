305779261f34026aa04250dab73a097a4c670d4f
Sarah Marie <sarwmsn@gmail.com>
2020-09-22 10:10:15 -0400

    variables

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-document.querySelectorAll(".btn").forEach((item) => {
-  item.addEventListener("click", () => {
-    alert("hello people");
-  });
-});
+let name = "john shrimp taco VI";
+let address, zip, state;
+address = "101 main street";
+zip = "60612";
+state = "CA";
+name = "fish burrito";
+console.log(address, zip, state);
+console.log(name);
