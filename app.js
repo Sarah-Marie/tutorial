f4ad788b8b5bc6d33f175f5f4a2b066fb20c5fb1
Sarah Marie <sarwmsn@gmail.com>
2020-09-24 12:29:06 -0400

    numbers

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-let name = "john shrimp taco VI";
-let address, zip, state;
-address = "101 main street";
-zip = "60612";
-state = "CA";
-name = "fish burrito";
-console.log(address, zip, state);
-console.log(name);
+//const number = 34;
+//let pants = 2.456;
+//pants = "are great";
+//const number2 = 2.456;
+
+//const add = number + number2;
+//const sub = number - number2;
+//const mult = number * number2;
+//const div = number / number2;
+//console.log(add);
+//console.log(sub);
+//console.log(mult);
+//console.log(div);
+
+let number = 40;
+number += 5;
+number -= 5;
+
+const slices = 10;
+const children = 4;
+//const amount = slices / children;
+const amount = slices % children;
+
+const random = 4 + 6 + 10 * 10;
+const random2 = (4 + 6 + 10) * 10;
+console.log(random);
+console.log(random2);
+console.log(amount);
+
+//console.log(number);
