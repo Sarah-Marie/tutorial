baeb17e13168479500a216fd0e095e7f9ea8e9bc
Sarah Marie <sarwmsn@gmail.com>
2020-12-19 13:31:48 -0500

    worked on elements

--- app.js
+++ app.js
@@ -91,9 +91,88 @@ console.log(betterItems); */
 
 // node-list = array-like objects
 // index, length property but not array methods
-
+/*
 const listItems = document.getElementsByClassName("special");
 
 console.log(listItems);
 
-listItems[2].style.color = "blue";
+listItems[2].style.color = "blue"; */
+
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+//querySelector('any css'); -selects single
+//querySelectorAll('any css')- selects all
+/*
+const result = document.querySelector('#result');
+result.style.backgroundColor = 'blue';
+
+const item = document.querySelector('.special');
+//console.log(item);
+
+const lastItem = document.querySelector("li:last-child");
+//console.log(lastItem);
+
+const list = document.querySelectorAll('.special');
+console.log(list);
+
+list.forEach(function(item){
+  console.log(item);
+  item.style.color = 'white';
+}); */
+
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+//childNodes - returns all childNodes including whitespace which is treated as a text node
+
+//children
+//firstChild
+//lastChild
+/*
+const result = document.querySelector('#result');
+const allChildren = result.childNodes;
+//console.log(allChildren);
+
+const children = result.children;
+console.log(children);
+
+console.log(result.firstChild);
+console.log(result.lastChild); */
+
+// parentElement;
+/*
+const heading = document.querySelector('h2');
+//console.log(heading.parentElement.parentElement.parentElement.parentElement);
+const parent = heading.parentElement;
+parent.style.color = 'red'; */
+
+// previousSibling
+// nextSibling
+//return whitespace
+/*
+const first = document.querySelector('.first');
+const second = (first.nextSibling.nextSibling.style.color = 'red');
+console.log(second);
+
+const last = document.querySelector('#last');
+const third = last.previousSibling.previousSibling;
+console.log(third);
+console.log(last.nextSibling.nextSibling); */
+
+//previousElementSibling
+//nextElementSibling
+/*
+const first = document.querySelector('.first');
+
+first.nextElementSibling.style.color = "red";
+const last = document.querySelector('#last'); */
+
+//nodeValue
+//textContent
+
+const item = document.getElementById('special');
+const value = item.firstChild.nodeValue;
+const easyValue = item.textContent;
+
+console.log(easyValue);
