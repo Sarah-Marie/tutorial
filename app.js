94a47a60fbe71a207e7deff5fe29d7d97f0a9360
Sarah Marie <sarwmsn@gmail.com>
2021-01-08 11:13:00 -0500

    worked with more event listeners

--- app.js
+++ app.js
@@ -430,7 +430,7 @@ btns.forEach(function(btn){
 // event propogation- order the events are fired
 // event bubbling- clicked element first then bubbles up- default
 // event capturing- fires at the root and fires until it reaches target
-
+/*
 const container = document.querySelector('.container');
 const list = document.querySelector('.list-items');
 
@@ -452,6 +452,29 @@ container.addEventListener('click', showBubbling, {capture: true});
 document.addEventListener('click', showBubbling, {capture: true});
 window.addEventListener('click', showBubbling, {capture: true});
 list.addEventListener('click', showBubbling, {capture: true});
+*/
 
+const container = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+//const heading = document.querySelector('.heading');
+//console.log(heading);
+
+function sayHello(){
+  console.log('Hello, there');
+}
+
+btn.addEventListener('click', function(){
+  const element = document.createElement('h1');
+  element.classList.add('heading');
+  element.textContent = `I'm inside the container`;
+  container.appendChild(element);
+});
+
+container.addEventListener('click', function(e){
+  if(event.target.classList.contains('heading')){
+    console.log('hello there');
+  }
+});
+//heading.addEventListener('click', sayHello);
 
 
